6cbe3cba7cd902224df1b68944ee98016b25d559
Gowri Shankar S <64461802+GowriShankar-engineer@users.noreply.github.com>
2022-07-15 03:34:18 +0530

    Update StudentMainPage.jsx

--- src/components/StudentMainPage.jsx
+++ src/components/StudentMainPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import "../styles/dashboardStyles1.css"
 import Dashboard from './Dashboard';
 import UpcomingExams from './UpcomingExams';
+import { MdOutlineSpaceDashboard, MdOutlineGridView } from 'react-icons/md'
 
 export default function StudentMainPage({ userInfo }) {
   const pages = {
@@ -17,13 +18,13 @@ export default function StudentMainPage({ userInfo }) {
     <div className='dashboard-container'>
       <>
         <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-          <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Online Exam Portal</a>
+          <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">🌀 Online Exam Portal</a>
           <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
           <div className="navbar-nav">
             <div className="nav-item text-nowrap">
-              <a className="nav-link px-3" href="#" onClick={()=>window.location.reload()}>Sign out</a>
+              <a className="nav-link px-4" href="#" style={{ color:"white", fontWeight:"inherit" }} onClick={()=>window.location.reload()}>Sign out</a>
             </div>
           </div>
         </header>
@@ -33,14 +34,14 @@ export default function StudentMainPage({ userInfo }) {
               <div className="position-sticky pt-3">
                 <ul className="nav flex-column">
                   <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#" onClick={()=> setPage('d')}>
-                      <span data-feather="home" />
+                    <a className="nav-link active" aria-current="page" href="#" onClick={()=> setPage('d')} style={{textAlign: 'left', paddingLeft:20}}>
+                    <MdOutlineSpaceDashboard size={20} className="icon-style-css" />
                       Dashboard
                     </a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="#" onClick={()=> setPage('er')}>
-                      <span data-feather="file" />
+                    <a className="nav-link" href="#" onClick={()=> setPage('er')} style={{textAlign: 'left', paddingLeft:20}}>
+                    <MdOutlineGridView size={20} className="icon-style-css" />
                       Exam Reports
                     </a>
                   </li>
